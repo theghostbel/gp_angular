5fa757fcb953109ffde3c2fa381a7a73f7ec48e1
k_zenchyk <k_zenchyk@wargaming.net>
2016-02-03 17:24:32 +0300

    * rewrite summ with underscore reduce

--- firstPrj/app/scripts/app.js
+++ firstPrj/app/scripts/app.js
@@ -1,13 +1,17 @@
+'use strict';
+
 angular.module('angularGPApp', [])
     .controller('summController',['$scope', function($scope){
 
-        $scope.summing = { firstN: "", secondN: ""};
+        $scope.firstN = "";
+        $scope.secondN = "";
+
         $scope.getSumm = function() {
-            return $scope.summing.firstN + $scope.summing.secondN;
+            var arr = [$scope.firstN, $scope.secondN];
+            var summ = _.reduce(arr, function(memo, num){return memo + num});
+            return summ;
         }
 
 
     }]);
-
-
 ;
