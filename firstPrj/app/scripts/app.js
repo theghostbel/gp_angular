b78a5a1de96c950ec062e5e74bb4da500e664e9b
k_zenchyk <k_zenchyk@wargaming.net>
2016-02-07 15:08:33 +0300

    Lesson 2:  add kt-sum directive

--- firstPrj/app/scripts/app.js
+++ firstPrj/app/scripts/app.js
@@ -1,17 +1,28 @@
 'use strict';
 
 angular.module('angularGPApp', [])
-    .controller('summController',['$scope', function($scope){
+
+
+    .directive('ktSum', function() {
+        /*let getSumm = function($scope) {
+            var arr = [$scope.firstN, $scope.secondN];
+            var summ = _.reduce(arr, function(memo, num) {
+                return memo + num
+            });
+            return summ;
+        }; */
+        return {
+            restrict: 'E',
+            scope: { sum: '@sum'},
+            template: "<input value='{{sum}}' readonly/>"
+
+        };
+    })
+
+    .controller('summController', ['$scope', function($scope) {
 
         $scope.firstN = "";
         $scope.secondN = "";
-
-        $scope.getSumm = function() {
-            var arr = [$scope.firstN, $scope.secondN];
-            var summ = _.reduce(arr, function(memo, num){return memo + num});
-            return summ;
-        }
-
 
     }]);
 ;
